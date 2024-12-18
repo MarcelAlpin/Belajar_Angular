93a94f0e1c0ed98e7333802ee1346d9664d1af2d
MarcelAlpin <marcelalpinsanty_2226240044@mhs.mdp.ac.id>
2024-12-18 09:37:35 +0700

    adding auth

--- belajar-angular/src/app/auth.guard.ts
+++ belajar-angular/src/app/auth.guard.ts
@@ -1,5 +1,22 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root',
+})
+export class authGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(): boolean {
+    const token = localStorage.getItem('authToken');
+
+    if (token) {
+      // Token ada, akses diizinkan
+      return true;
+    } else {
+      // Token tidak ada, arahkan ke halaman login
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
